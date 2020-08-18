6910b259e9403a3bc4b790b3d62118672f21bad1
Pavan Sharma <sharmapavan369@gmail.com>
2020-08-18 22:21:52 +0530

    move impl to separate package

--- src/sort/Runner.java
+++ src/sort/Runner.java
@@ -1,18 +1,20 @@
 package sort;
 
+import sort.impl.InsertionSort;
+
 public class Runner {
-	
-	public static void main(String[] args){
-		Sort sorter=new InsertionSort();
-		Integer[] i={55,3,96,4,44,2,65,29,23};
-		for(Integer j:i){
-			System.out.print(j+" ");
-		}
-		System.out.println();
-		sorter.sort(i);
-		for(Integer j:i){
-			System.out.print(j+" ");
-		}
-	}
+
+  public static void main(String[] args) {
+    final Sort sorter = new InsertionSort();
+    final Integer[] i = {55, 3, 96, 4, 44, 2, 65, 29, 23};
+    for (final Integer j : i) {
+      System.out.print(j + " ");
+    }
+    System.out.println();
+    sorter.sort(i);
+    for (final Integer j : i) {
+      System.out.print(j + " ");
+    }
+  }
 
 }
---
+++ src/sort/impl/InsertionSort.java
@@ -0,0 +1,20 @@
+package sort.impl;
+
+import sort.Sort;
+
+public class InsertionSort implements Sort {
+
+  @Override
+  public <T extends Comparable<T>> void sort(T[] values) {
+    for (int out = 1; out < values.length; out++) {
+      final T temp = values[out];
+      int in = out;
+      while (in > 0 && values[in - 1].compareTo(temp) > 0) {
+        values[in] = values[in - 1];
+        --in;
+      }
+      values[in] = temp;
+    }
+  }
+
+}
